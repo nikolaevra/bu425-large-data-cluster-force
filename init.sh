647b918bf5fb9cfdc703d21baf7c4211034cc852
Ruslan Nikolaev <nikolaevra@gmail.com>
2019-02-07 17:28:40 -0500

    upgrading init file

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 sudo apt install python-pip
-pip install virtualenv
+pip install --user virtualenv
+
+sudo apt-get -y install ipython ipython-notebook
+pip install --user jupyter
+
 virtualenv env
 source env/bin/activate
+pip install --upgrade pip 
 pip install requirements.txt
 
